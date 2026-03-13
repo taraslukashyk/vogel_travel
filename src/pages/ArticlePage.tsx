f8562783f534b9db44451ee537d1bf9c24779d72
taraslukashyk <taraslukashik@gmail.com>
2026-03-13 21:10:36 +0200

    помилка деплою виправлена

--- src/pages/ArticlePage.tsx
+++ src/pages/ArticlePage.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from 'react';
 import { useParams, Link, useLocation } from 'react-router-dom';
-import { ArrowLeft, Calendar, Share2 } from 'lucide-react';
+import { ArrowLeft, Share2 } from 'lucide-react';
 import { blogPosts } from '../data/blog';
 
 const ArticlePage = () => {
