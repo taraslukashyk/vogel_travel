bf7d890780caed2455411ac3336e341b4a90ee43
taraslukashyk <taraslukashyk@users.noreply.github.com>
2026-03-16 09:13:32 +0000

    deploy: 75a1eadb463dcc24368b0cb926a2f783df7d69fa

--- assets/OptimizedImage-DTArbyI4.js
+++ assets/OptimizedImage-DTArbyI4.js
@@ -1,4 +1,4 @@
-import{j as w}from"./index-CX1IOKgy.js";import"./vendor-react-CUfvpDno.js";const n=({src:o,alt:i,className:s,sizes:r="(max-width: 768px) 100vw, 50vw",...m})=>{const p=o.includes("images.unsplash.com");let a;if(p){const t=o.split("?")[0];a=`
+import{j as w}from"./index-k-ZKVMRN.js";import"./vendor-react-CUfvpDno.js";const n=({src:o,alt:i,className:s,sizes:r="(max-width: 768px) 100vw, 50vw",...m})=>{const p=o.includes("images.unsplash.com");let a;if(p){const t=o.split("?")[0];a=`
       ${t}?auto=format&fit=crop&q=60&w=400 400w,
       ${t}?auto=format&fit=crop&q=70&w=800 800w,
       ${t}?auto=format&fit=crop&q=80&w=1200 1200w,
