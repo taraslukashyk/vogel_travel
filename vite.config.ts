800f651e987d7b2fd58e0948d34b44ede84f8549
taraslukashyk <taraslukashik@gmail.com>
2026-03-26 14:24:52 +0200

    виправив vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vogel_travel/',
+  base: '/',
   build: {
     rollupOptions: {
       output: {
