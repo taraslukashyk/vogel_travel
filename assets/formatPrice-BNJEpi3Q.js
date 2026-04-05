668a55ec550c53e7cf8bd8caba3d71b483fd83f7
taraslukashyk <taraslukashyk@users.noreply.github.com>
2026-04-05 19:42:59 +0000

    deploy: 7c9811575a6b329bbebee303180ec6c0cb16d3ee

---
+++ assets/formatPrice-BNJEpi3Q.js
@@ -0,0 +1,1 @@
+const o=t=>{if(t==null)return"0";const n=typeof t=="string"?parseFloat(t):t;return isNaN(n)?"0":n.toLocaleString("en-US").replace(/,/g," ")};export{o as f};
---
+++ assets/html-Co27hwkg.js
@@ -0,0 +1,1 @@
+const a=e=>e.replace(/&/g,"&amp;").replace(/</g,"&lt;").replace(/>/g,"&gt;");export{a as e};
