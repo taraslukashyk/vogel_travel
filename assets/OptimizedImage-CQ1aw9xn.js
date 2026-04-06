040a9d2ef0bb0d835917cdb7a72a0ea4bf508820
taraslukashyk <taraslukashyk@users.noreply.github.com>
2026-04-06 11:51:35 +0000

    deploy: f03963bdcb908b19c0963540905a7f0f05a39f3b

---
+++ assets/OptimizedImage-CQ1aw9xn.js
@@ -0,0 +1,6 @@
+import{j as e}from"./index-YfB01bKi.js";const w=({src:o,alt:i,className:s,sizes:r="(max-width: 768px) 100vw, 50vw",...m})=>{const p=o.includes("images.unsplash.com");let a;if(p){const t=o.split("?")[0];a=`
+      ${t}?auto=format&fit=crop&q=60&w=400 400w,
+      ${t}?auto=format&fit=crop&q=70&w=800 800w,
+      ${t}?auto=format&fit=crop&q=80&w=1200 1200w,
+      ${t}?auto=format&fit=crop&q=80&w=1600 1600w
+    `}return e.jsx("img",{src:o,alt:i,className:s,loading:"lazy",decoding:"async",srcSet:a,sizes:r,...m})};export{w as O};
